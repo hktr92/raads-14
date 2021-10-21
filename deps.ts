4424eb37c4cb4f68d7ed1cb064c40468c5f9dedf
Petru Szemereczki <szemereczkipetru@gmail.com>
2021-10-21 22:38:05 +0300

    - updated architecture

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
+export { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
--- mod.tsx
+++ mod.tsx
@@ -4,30 +4,17 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
-import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
+import { h, listenAndServe, renderSSR } from "./deps.ts";
+import { App } from "./src/App.tsx";
 
-function App() {
-  return (
-    <html>
-      <head>
-        <title>Hello from JSX</title>
-      </head>
-      <body>
-        <h1>Hello world</h1>
-      </body>
-    </html>
-  );
-}
-
-function handler(req: Request) {
+const handler = async (req: Request): Promise<Response> => {
   const html = renderSSR(<App />);
   return new Response(html, {
     headers: {
       "content-type": "text/html",
     },
   });
-}
+};
 
 console.log("Listening on http://localhost:8080");
 await listenAndServe(":8080", handler);
